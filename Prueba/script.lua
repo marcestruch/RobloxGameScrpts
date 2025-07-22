ba6cb1c7ddfd7eee791b5b3c2857b8a99f657e4a
marcestruch <marcstruchmalonda@gmail.com>
2025-07-22 15:09:57 +0200

    Cambios en los dos

--- Prueba/script.lua
+++ Prueba/script.lua
@@ -6,17 +6,34 @@ local coin = proximityPrompt.Parent
 -- Crear funcion
 
 proximityPrompt.Triggered:Connect(function(player)
-    -- Acceder al usuario que interactua con la moneda
-    local Player = game.Players:FindFirstChild(player.Name)
-    -- 
+	
+	-- Acceder al usuario que interactua con la moneda
+	
+	local Player = game.Players:FindFirstChild(player.Name)
+	
+	-- Acceder a leaderstats
 
-    -- Al interactuar, hacemos la pieza transparente
-    coin.Transparency = 1
-    coin.Decal1.Transparency = 1
-    coin.Decal2.Transparency = 1
-    -- Desactivar proximityPrompt
-    proximityPrompt.Enabled = false
-    -- Al interactuar quitar colision
-    coin.CanCollide = false
-
+	local leaderstats = Player:WaitForChild("leaderstats")
+	
+	-- Acceder a variable coin
+	
+	local Coin = leaderstats:FindFirstChild("Coin")
+	
+	-- Sumar 1 a la variable coin
+	
+	Coin.Value += 1;
+	
+	-- Desactivar proximityPrompt
+	
+	proximityPrompt.Enabled = false
+	
+	-- Al interactuar, hacemos la pieza transparente
+	
+	coin.Transparency = 1
+	coin.Decal1.Transparency = 1
+	coin.Decal2.Transparency = 1
+	
+	-- Al interactuar quitar colision
+	
+	coin.CanCollide = false
 end)
